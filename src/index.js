4be1e3d7bfe242683e4c643e1e062386df2d4645
lestgabo <lestgabo@gmail.com>
2021-01-06 16:35:10 -0800

    Add material ui theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: "'Fira Code', 'monospace'",
+        fontSize: 14,
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+    },
+    palette: {
+        primary: {
+            main: '#1E90FF',
+            // light: '#77769A',
+        },
+        secondary: {
+            main: '#008000',
+            // light: '#C96471',
+        },
+    },
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
